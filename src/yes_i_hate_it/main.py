772679c1e2fd9ad7222949f4a9d2c7e770439558
David Lopez Saez <davidlopez.hellin@outlook.com>
2022-05-18 00:56:20 +0200

    Implement get_tweets
    * get tweets function, missing exceptions handling
    
    * Change github action environment
    
    * Fix environment secrets on github actions

--- src/yes_i_hate_it/main.py
+++ src/yes_i_hate_it/main.py
@@ -5,7 +5,11 @@ Main file which will handle:
     - if it is football related, fetch data from a random API
     - respond with the fetch data
 """
+from typing import List
 import os
+import tweepy
+
+from yes_i_hate_it.exceptions import ValueExceeded, ValueInferior
 
 
 def load_env():
@@ -19,8 +23,39 @@ def load_env():
     }
     return tokens
 
+
+def get_tweets(user_name: str, max_results: int) -> List[tweepy.Tweet]:
+    """Get 'x' amount of latests tweets from 'y' user"""
+    # test max_result value
+    if max_results < 5:
+        raise ValueInferior
+    if max_results > 100:
+        raise ValueExceeded
+
+    # init tweepy client object to call Twitter REST API
+    tokens = load_env()
+    client = tweepy.Client(tokens[TWITTER_BEARER_TOKEN])
+
+    # get user from given user_name
+    user = client.get_user(username=user_name)
+
+    # get latest amount of tweets from user_name
+    tweets = client.get_users_tweets(id=user.data['id'], max_results=max_results)
+    return list(tweets.data)
+
+
+def process_tweets(_tweets):
+    """
+    Get twitter raw API from tweets and returns a list fo tweets with:
+        - tweet body
+        - tweet id
+        - tweet user id
+    """
+
+
 def main():
     """main function"""
+    # print(get_tweets('Javieff16YT', 30))
 
 
 TWITTER_API_KEY = 'TWITTER_API_KEY'
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,7 +1,13 @@
 """Tests for main.py"""
+import pytest
+import tweepy
+
+from yes_i_hate_it.main import get_tweets
 from yes_i_hate_it.main import load_env
 from yes_i_hate_it.main import TWITTER_API_KEY, TWITTER_API_SECRET
 from yes_i_hate_it.main import TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET, TWITTER_BEARER_TOKEN
+
+from yes_i_hate_it.exceptions import ValueExceeded, ValueInferior
 
 
 def test_load_env():
@@ -23,3 +29,20 @@ def test_load_env():
 
     assert TWITTER_BEARER_TOKEN in tokens
     assert isinstance(tokens[TWITTER_BEARER_TOKEN], str)
+
+
+def test_get_tweets():
+    """Test main.get_tweets"""
+    max_results = 10
+    user_name = 'javieff16YT'
+    tweets = get_tweets(user_name, max_results)
+
+    assert isinstance(tweets, list)
+    assert len(tweets) == max_results - 1
+    assert isinstance(tweets[0], tweepy.tweet.Tweet)
+
+    with pytest.raises(ValueExceeded):
+        get_tweets(user_name, 102)
+
+    with pytest.raises(ValueInferior):
+        get_tweets(user_name, 1)
