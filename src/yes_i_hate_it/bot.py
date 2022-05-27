6a749e9a1b40b01d830b29a178a9854bb2985296
inigo183 <inigozarate01@gmail.com>
2022-05-27 16:27:42 +0200

    discord logs, footbal in data base

--- src/yes_i_hate_it/bot.py
+++ src/yes_i_hate_it/bot.py
@@ -10,7 +10,7 @@ from discord.ext import commands
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-from yes_i_hate_it.config import DISCORD_TOKEN, CATEGORY_ID
+from yes_i_hate_it.config import DISCORD_TOKEN, CATEGORY_ID, DISCORD_LOG_FILE
 from yes_i_hate_it.config import TWEETS_DB_PATH
 from yes_i_hate_it.process_tweets import Tweet
 
@@ -29,13 +29,10 @@ class MyBot(commands.Bot):
         commands.Bot.__init__(self, command_prefix=command_prefix, self_bot=self_bot)
         self.add_commands()
 
-    # pylint:disable = logging-not-lazy
     async def on_ready(self):
         """Execute when bot is succesfully connected"""
-        print(f'{self.user.name} has connected to Discord!')
-        logging.info(self.user.name + ' has connected to Discord!')
-        guild = self.guilds[1].text_channels
-        print(guild)
+        logging.info('%s has connected to Discord!', self.user.name)
+        guild = self.guilds[0].text_channels
         channels = []
         session = self.session_maker()
 
@@ -48,9 +45,8 @@ class MyBot(commands.Bot):
 
             msgs = await channel.history(limit=100).flatten()
             if not msgs:
-                print('None')
                 continue
-            logging.info('found msgs in lobby-' + name[1])
+            logging.info('found msgs in lobby-%s', name[1])
             for msg in msgs:
                 splitted_msg = msg.content.split()
                 tweet = session.query(Tweet).get(int(splitted_msg[1]))
@@ -60,14 +56,14 @@ class MyBot(commands.Bot):
             session.commit()
             await channel.purge()
             channels.append(channel)
-            logging.info('Unrequested and deleted messages in lobby-' + name[1])
+            logging.info("Unrequested and deleted messages in lobby-%s", name[1])
 
 
     def add_commands(self):
         """add commands to bot"""
         def read_10():
             """Read 10 tweets from data base"""
-            logging.info('giving 10 new tweets')
+            logging.info("giving 10 new tweets")
             session = self.session_maker()
             # pylint:disable = singleton-comparison
             tweets = session.query(Tweet).filter(Tweet.requested==False).limit(10).all()
@@ -82,7 +78,7 @@ class MyBot(commands.Bot):
         @self.command(name="list", pass_context=True)
         async def list_tweets(ctx):
             """Start sending data"""
-            logging.info(ctx.author.name + 'requested list')
+            logging.info("%s requested list", ctx.author.name)
             name = f'lobby-{ctx.author.name}'
             channel_id = [g.id for g in ctx.message.guild.channels if g.name == name.lower()]
             msgs = read_10()
@@ -94,7 +90,7 @@ class MyBot(commands.Bot):
         @self.command(name="addme", pass_context=True)
         async def add_me(ctx):
             """Create channel for user and check it is not already created"""
-            logging.info(ctx.author.name + 'rquested add me')
+            logging.info("%s rquested add me", ctx.author.name)
             guild = ctx.message.guild
             name = f'lobby-{ctx.author.name}'
             group = [g for g in ctx.message.guild.channels if g.id == CATEGORY_ID]
@@ -109,7 +105,7 @@ class MyBot(commands.Bot):
             corr = True
             for msg in msgs:
                 corr = corr and (sum([react.count for react in msg.reactions]) == 3)
-            logging.info('messages checked as' + str(corr))
+            logging.info("messages checked as %s", str(corr))
             return corr
 
         def clasify_data(msgs):
@@ -121,14 +117,14 @@ class MyBot(commands.Bot):
                 if str(msg.reactions[0].emoji) == ('✅') and msg.reactions[0].count == 2:
                     tweet.is_football = True
                     session.add(tweet)
-                    logging.info('clasified ' + str(tweet.tweet_id) + ' as football')
+                    logging.info("clasified %i as football", tweet.tweet_id)
 
             session.commit()
 
         @self.command(name="c", pass_context=True)
         async def confirm(ctx):
             """Delete all messages in users channel"""
-            logging.info(ctx.author.name + 'requeste confirmation')
+            logging.info("%s request confirmation", ctx.auto.name)
             messages = await ctx.channel.history(limit=100).flatten()
             messages = [msg for msg in messages if '>' != msg.content[0]]
             if correct(messages):
@@ -149,5 +145,13 @@ def main():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='process.log', format='%(asctime)s %(message)s', level=logging.INFO, force=True)
+    DISCORD_LOG_FILE.parents[0].mkdir(exist_ok=True)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.FileHandler(DISCORD_LOG_FILE),
+            logging.StreamHandler()
+        ]
+    )
     main()
