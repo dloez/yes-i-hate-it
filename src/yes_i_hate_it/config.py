9f3828880b6e83b89b68caf5b2bf8ebb3e477eac
David Lopez <davidlopez.hellin@outlook.com>
2022-06-06 00:42:45 +0200

    Add multiprocessing

--- src/yes_i_hate_it/config.py
+++ src/yes_i_hate_it/config.py
@@ -44,7 +44,7 @@ LOGS_PATH = Path('logs')
 LOG_FILE = LOGS_PATH / 'main.log'
 DISCORD_LOG_FILE = LOGS_PATH / 'discord_bot.log'
 GATHER_LOG_FILE = LOGS_PATH / 'gather.log'
-BOW_LOG_FILE = LOGS_PATH / 'bow.log'
+CLUSTER_LOG_FILE = LOGS_PATH / 'cluster.log'
 DATASET_LOG_FILE = LOGS_PATH / 'dataset.log'
 
 SLEEP_TIME_MINUTES = 5
@@ -66,6 +66,10 @@ DISCORD_WEBHOOK = 'https://discord.com/api/webhooks/976894637438554143/f1rOdQUB-
 DISCORD_TOKEN = 'DISCORD_TOKEN'
 CATEGORY_ID = 979508900342685696
 # CATEGORY_ID = 979112708530139200
+
+# clustering
+WORD2VEC_MODEL_PATH = DATA_PATH / 'word2vec.model'
+WORD2VEC_WV_PATH = DATA_PATH / 'word2vec.wordvectors'
 
 # Dataset
 DATASET_FILE_PATH = DATA_PATH / 'dataset.npz'
--- src/yes_i_hate_it/generate_bow.py
+++ src/yes_i_hate_it/generate_bow.py
@@ -1,10 +1,14 @@
 """Process tweets and generate bag of worlds"""
 import re
+import sys
+import random
 import logging
 import nltk
 import stanza
 import collections
-from gensim.models import Word2Vec
+from functools import lru_cache
+import multiprocessing as mp
+from gensim.models import Word2Vec, KeyedVectors
 from unidecode import unidecode
 from nltk.corpus import stopwords
 from sqlalchemy.orm import sessionmaker
@@ -16,7 +20,8 @@ from yes_i_hate_it.gather_tweets import Tweet
 
 from yes_i_hate_it.config import TWEETS_DB_PATH
 from yes_i_hate_it.config import BASE
-from yes_i_hate_it.config import BOW_LOG_FILE
+from yes_i_hate_it.config import CLUSTER_LOG_FILE
+from yes_i_hate_it.config import WORD2VEC_MODEL_PATH, WORD2VEC_WV_PATH
 
 
 def get_tweets(session, amount):
@@ -53,64 +58,142 @@ def process_text(text):
     return words
 
 
-def generate_chunks(sentences, keyed_vectors):
-    """Generate group of words called 'chunks'"""
-    chunks = {}
-    new_chunk = 0
+def process_word(word, clusters, queue):
+    """Return if a word should be added to a cluster or create a new one"""
+    action = {'word': word, 'name': ''}
+    word_vectors = KeyedVectors.load(str(WORD2VEC_WV_PATH), mmap='r')
+
+    # first batch of words will trigger a new cluster creation for all of them
+    # should this be handled? does it really matter?
+    if not clusters:
+        action['name'] = NEW_CLUSTER
+        queue.put(action)
+        return
+                
+    best_similarity = 0
+    best_cluster = ''
+    found_in = ''
+    for key, cluster_words in clusters.items():
+        similarity, is_found = get_cluster_similarity(word_vectors, cluster_words, word)
+        if is_found:
+            found_in = key 
+
+        if not best_similarity:
+            best_similarity = similarity
+            best_cluster = key
+            continue
+
+        if similarity > best_similarity:
+            best_similarity = similarity
+            best_cluster = key
+
+    if best_similarity >= MIN_SIMILARITY:
+        action['data'] = {}
+        if found_in:
+            action['name'] = SWAP
+            action['data']['from_cluster'] = found_in
+            action['data']['to_cluster'] = best_cluster
+        else:
+            action['name'] = ADD
+            action['data']['to_cluster'] = best_cluster
+    else:
+        action['name'] = NEW_CLUSTER
+    queue.put(action)
+
+
+def chunk_words(words, size):
+    """Split words set into small chunks"""
+    return (words[pos:pos+size] for pos in range(0, len(words), size))
+    
+
+def generate_clusters(words):
+    """Generate group of words called 'clusters'"""
+    # instantiate cluster dictionary on a shared memory block
+    manager = mp.Manager()
+    clusters = manager.dict()
+
+    # instantiate queue for multiprocessing communication
+    queue = mp.Queue()
+
+    new_cluster = 0
+    size = mp.cpu_count()
+
     for i in range(EPOCHS):
-        logging.info('Epoch %s', i)
-        for sentence in sentences:
-            for word in sentence:
-                if not chunks:
-                    key = f'chunk_{new_chunk}'
-                    chunks[key] = []
-                    chunks[key].append(word)
-                    new_chunk += 1
-                    continue
-                
-                found_in = find_word(word, chunks)
-                if found_in:
-                    chunks[found_in].remove(word)
-                    if not chunks[found_in]:
-                        chunks.pop(found_in)
-                
-                best_similarity = 0
-                best_chunk = ''
-                for key, words in chunks.items():
-                    similarity = get_chunk_similarity(keyed_vectors, words, word)
+        logging.warning('Epoch %s', i+1)
+        random.shuffle(words)
+        current_words = 0
+        for x, chunk in enumerate(chunk_words(words, size)):
+            current_words += len(chunk)
+            processes = []
+            for word in chunk:
+                process = mp.Process(target=process_word, args=(word, clusters, queue))
+                process.start()
+                processes.append(process)
+            
+            returns = []
+            for process in processes:
+                returns.append(queue.get())
 
-                    if not best_similarity:
-                        best_similarity = similarity
-                        best_chunk = key
-                        continue
+            for process in processes:
+                process.join()
+            
+            for ret in returns:
+                if ret['name'] == NEW_CLUSTER:
+                    key = f'C{new_cluster}'
+                    clusters[key] = manager.list()
+                    clusters[key].append(ret['word'])
+                    new_cluster += 1
+                elif ret['name'] == ADD:
+                    cluster_key = ret['data']['to_cluster']
+                    clusters[cluster_key].append(ret['word'])
+                elif ret['name'] == SWAP:
+                    from_cluster = ret['data']['from_cluster']
+                    to_cluster = ret['data']['to_cluster']
 
-                    if similarity > best_similarity:
-                        best_similarity = similarity
-                        best_chunk = key
+                    # delete word from current cluster holder and delete cluster if it has no words
+                    clusters[from_cluster].remove(ret['word'])
+                    if not clusters[from_cluster]:
+                        clusters.pop(from_cluster)
 
-                if best_similarity >= MIN_SIMILARITY:
-                    if word not in chunks[best_chunk]:
-                        chunks[best_chunk].append(word)
+                    # add word to cluster
+                    clusters[to_cluster].append(ret['word'])
+  
+            hashtags_amount = round(current_words * 20 / len(words)) 
+            hashtags = '['
+            for i in range(1, 20):
+                if i < hashtags_amount:
+                    hashtags += '#'
                 else:
-                    key = f'chunk_{new_chunk}'
-                    chunks[key] = []
-                    chunks[key].append(word)
-                    new_chunk += 1
-    print(chunks)
-    print(len(chunks))
+                    hashtags += ' '
+            hashtags += ']'
+            print(f'Words: {current_words}/{len(words)} || Clusters: {len(clusters)} || {hashtags}', end='\r')
+            sys.stdout.flush() 
+        print('')
+        
 
-
-def get_chunk_similarity(keyed_vectors, chunk, word):
+@lru_cache
+def get_cluster_similarity(word_vectors, cluster_words, word):
     """Return average similary between words """
     similarity = 0
-    for chunk_word in chunk:
-        similarity += keyed_vectors.similarity(word, chunk_word)
-    return similarity / len(chunk)
+    # create copy of list to avoid modifying list on multiprocessing
+    cluster_words = list(cluster_words)
+    
+    is_found = False
+    if word in cluster_words:
+        cluster_words.remove(word)
+        is_found = True
+
+    if not cluster_words:
+        return -1, is_found
+
+    for cluster_word in cluster_words:
+        similarity += word_vectors.similarity(word, cluster_word)
+    return similarity / len(cluster_words), is_found
 
 
-def find_word(word, chunks):
+def find_word(word, clusters):
     """Find word across all chunks and if found return chunk key"""
-    for key, words in chunks.items():
+    for key, words in clusters.items():
         if word in words:
             return key
 
@@ -118,12 +201,12 @@ def find_word(word, chunks):
 def main():
     """Main function"""
     # pylint: disable = no-member
-    BOW_LOG_FILE.parents[0].mkdir(exist_ok=True)
+    CLUSTER_LOG_FILE.parents[0].mkdir(exist_ok=True)
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.WARNING,
         format="%(asctime)s - %(levelname)s - %(message)s",
         handlers=[
-            logging.FileHandler(BOW_LOG_FILE),
+            logging.FileHandler(CLUSTER_LOG_FILE),
             logging.StreamHandler()
         ]
     )
@@ -137,24 +220,40 @@ def main():
     processed_tweets = []
     while tweets:
         for tweet in tweets:
-            logging.info("Processing tweet with ID: %s", tweet.tweet_id)
+            logging.warning("Processing tweet with ID: %s", tweet.tweet_id)
             processed_tweets.append(process_text(tweet.text))
         tweets = get_tweets(session, 10)
     
     model =  Word2Vec(min_count=1, epochs=1, vector_size=100)
     model.build_vocab(processed_tweets)
+    model.save(str(WORD2VEC_MODEL_PATH))
 
-    keyed_vectors = model.wv
+    word_vectors = model.wv
+    word_vectors.save(str(WORD2VEC_WV_PATH))
     del model
-    generate_chunks(processed_tweets, keyed_vectors)
+
+    # delete repeated words
+    clean_words = set()
+    for sentence in processed_tweets:
+        for word in sentence:
+            clean_words.add(word)
+    del processed_tweets
+    
+    clean_words = list(clean_words)
+    generate_clusters(clean_words)
 
 
 # download stopwords
 nltk.download('stopwords')
 nlp = stanza.Pipeline(lang='es', processors='tokenize,mwt,pos,lemma')
 
-EPOCHS = 50
+EPOCHS = 10
 MIN_SIMILARITY = 0.3
+
+# Acctions returned from processes
+ADD = 'ADD'
+SWAP = 'SWAP'
+NEW_CLUSTER = 'NEW_CLUSTER'
 
 if __name__ == '__main__':
     main()
