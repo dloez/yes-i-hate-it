422abb9ffc7b8aa0fd50488d5140c342bf2d5038
inigo183 <inigozarate01@gmail.com>
2022-06-08 03:16:35 +0200

    add create cluster, with data plots

--- src/yes_i_hate_it/generate_cluster.py
+++ src/yes_i_hate_it/generate_cluster.py
@@ -5,15 +5,13 @@ import random
 import logging
 import nltk
 import stanza
-import collections
 import multiprocessing as mp
 from gensim.models import Word2Vec, KeyedVectors
 from unidecode import unidecode
 from nltk.corpus import stopwords
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.exc import IntegrityError
+import matplotlib.pyplot as plt
 
 from yes_i_hate_it.gather_tweets import Tweet
 
@@ -26,11 +24,11 @@ from yes_i_hate_it.config import WORD2VEC_MODEL_PATH, WORD2VEC_WV_PATH
 def get_tweets(session, amount):
     """Return n amount of not processed tweets from database"""
     # pylint: disable = singleton-comparison
-    tweets = session.query(Tweet).filter(Tweet.processed==False).limit(amount).all()
-    for tweet in tweets:
-        tweet.processed = True
-    session.add_all(tweets)
-    session.commit()
+    tweets = session.query(Tweet).filter(Tweet.processed==True).limit(amount).all()
+    # for tweet in tweets:
+    #     tweet.processed = True
+    # session.add_all(tweets)
+    # session.commit()
     return tweets
 
 
@@ -55,6 +53,129 @@ def process_text(text):
             if lemma not in stop_words:
                 words.append(unidecode(lemma))
     return words
+
+
+def create_clusters(words):
+    """Create clusters"""
+    word_vectors = KeyedVectors.load(str(WORD2VEC_WV_PATH), mmap='r')
+    # c1 = {'words': [], 'mean_similarity': 0}
+    clusters = {} #{'c1': c1}
+    clustered_words = {} #{'porterazos': 'c1'}
+    new_cluster = 1
+    print('start for')
+    for word in words:
+        word_similarities = max_normalized_sim(word_vectors, word)
+        best_similarity = 0
+        for sim in word_similarities:
+            if sim[0] in clustered_words:
+                similarity = sim[1] * clusters[clustered_words[sim[0]]]['mean_similarity']
+            else:
+                similarity = sim[1]
+            if similarity > best_similarity:
+                best_similarity = similarity
+                word_to_append = sim[0]
+
+        # if word in clustered_words:
+        #     word_cluster = clustered_words[word]
+        #     if word_to_append in clustered_words:
+        #         if word_cluster == clustered_words[word_to_append]:
+        #             continue
+
+        #     clusters[word_cluster]['words'].remove(word)
+        #     clusters[word_cluster]['mean_similarity'] = calculate_mean_similarity(word_vectors, clusters[word_cluster]['words'])
+
+        if word_to_append in clustered_words:
+            cluster = clustered_words[word_to_append]
+            clustered_words[word] = cluster
+            if word not in clusters[cluster]['words']:
+                clusters[cluster]['words'].append(word)
+            clusters[cluster]['mean_similarity'] = calculate_mean_similarity(word_vectors, clusters[cluster]['words'])
+        else:
+            cluster_name = f'c{new_cluster}'
+            clustered_words[word] = cluster_name
+            clustered_words[word_to_append] = cluster_name
+            cluster = {'words': [word, word_to_append], 'mean_similarity': best_similarity}
+            clusters[cluster_name] = cluster
+            new_cluster += 1
+
+        print(
+            f"Words: {len(clustered_words)}/{len(words)} || Clusters: {len(clusters)} || {proportional_hashtags(len(clustered_words),len(words))}",
+            end='\r'
+        )
+        sys.stdout.flush()
+    print('')
+    # print(clustered_words)
+    # print(clusters)
+    represent_data(clusters)
+
+
+def max_normalized_sim(word_vectors, word):
+    """Return normalized vector of maximum similarities"""
+    similarities = word_vectors.most_similar(word)
+    normalized_sim = []
+    for sim in similarities:
+        normalized_sim.append((sim[0], (sim[1]+1)/2))
+
+    return normalized_sim
+
+
+def calculate_mean_similarity(word_vectors, words):
+    """Return mean simalrity in clusters"""
+    mean = 0
+    total_words = len(words)
+    for i, word in enumerate(words):
+        mean += sum([word_vectors.similarity(word, words[j]) for j in range(i+1, total_words)]) / (total_words - i)
+    mean /= total_words
+
+    return mean
+
+
+def proportional_hashtags(current_words, total_words):
+    """Return string with progresson bar"""
+    hashtags_amount = round(current_words * 20 / total_words) 
+    hashtags = '['
+    for i in range(1, 20):
+        if i < hashtags_amount:
+            hashtags += '#'
+        else:
+            hashtags += ' '
+    hashtags += ']'
+
+    return hashtags
+
+
+def represent_data(clusters):
+    """Create and save plots with cluster data"""
+    x = []
+    n = []
+    means = []
+    for i, cluster in enumerate(clusters):
+        x.append(i+1)
+        print(clusters[cluster]['words'])
+        n.append(len(clusters[cluster]['words']))
+        means.append(clusters[cluster]['mean_similarity'])
+
+    normalize_n = [float(i)/max(n) for i in n]
+
+    plt.plot(x, normalize_n, 'r', label='n elements')
+    plt.plot(x, means, 'b', label='means')
+    plt.legend(loc='upper left')
+    plt.xlabel('Cluster')
+    plt.ylabel('n elements')
+    plt.savefig('data/elem_sim.png')
+    plt.clf()
+
+    plt.plot(x, n)
+    plt.xlabel('Cluster')
+    plt.ylabel('n elements')
+    plt.savefig('data/elements.png')
+    plt.clf()
+
+    plt.plot(x, means)
+    plt.xlabel('Cluster')
+    plt.ylabel('mean similarity')
+    plt.savefig('data/similarity.png')
+    plt.clf()
 
 
 def process_word(actions_queue, words_queue, poisson_queue, clusters):
@@ -107,7 +228,7 @@ def process_word(actions_queue, words_queue, poisson_queue, clusters):
 def chunk_words(words, size):
     """Split words set into small chunks"""
     return (words[pos:pos+size] for pos in range(0, len(words), size))
-    
+
 
 def generate_clusters(words):
     """Generate group of words called 'clusters'"""
@@ -168,7 +289,7 @@ def generate_clusters(words):
 
                     # add word to cluster
                     clusters[to_cluster].append(ret['word'])
-  
+
             hashtags_amount = round(current_words * 20 / len(words)) 
             hashtags = '['
             for i in range(1, 20):
@@ -226,14 +347,12 @@ def main():
     session_maker = sessionmaker(bind=engine)
     session = session_maker()
 
-    tweets = get_tweets(session, 10)
+    tweets = get_tweets(session, 100)
     processed_tweets = []
-    while tweets:
-        for tweet in tweets:
-            logging.warning("Processing tweet with ID: %s", tweet.tweet_id)
-            processed_tweets.append(process_text(tweet.text))
-        tweets = get_tweets(session, 10)
-    
+    for tweet in tweets:
+        logging.warning("Processing tweet with ID: %s", tweet.tweet_id)
+        processed_tweets.append(process_text(tweet.text))
+
     model =  Word2Vec(min_count=1, epochs=1, vector_size=100)
     model.build_vocab(processed_tweets)
     model.save(str(WORD2VEC_MODEL_PATH))
@@ -250,7 +369,7 @@ def main():
     del processed_tweets
     
     clean_words = list(clean_words)
-    generate_clusters(clean_words)
+    create_clusters(clean_words)
 
 
 # download stopwords
